aae345d5044b0328ccc53542960e6a04b77d9561
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-10-29 01:17:47 +0300

    refactor: remove fucking_great_advice.py from root (moved to src/)

--- fucking_great_advice.py
+++
@@ -1,9 +0,0 @@
-import requests
-class Client():
-	def __init__(self):
-		self.api="https://fucking-great-advice.ru/api/v2"
-		self.headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"}
-	def random_advices(self):
-		return requests.get(f"{self.api}/random-advices",headers=self.headers).json()
-	def random_advices_by_tag(self,tag):
-		return requests.get(f"{self.api}/random-advices-by-tag?tag={tag}",headers=self.headers).json()
